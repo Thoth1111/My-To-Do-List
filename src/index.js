e222dc30a2aac28770743da2ee94e3454964d227
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-01-20 14:44:28 +0300

    Add code for updating list items

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ class Manager {
       `<div class="entry">
         <div>
         <input type="checkbox" class="boxes" name="status" id="status">
-        <input type="text" size="125" class="detail" value="${entry.description}"></input>
+        <input type="text" size="125" class="detail" id="${entry.index}" value="${entry.description}"></input>
         </div>
         <img src='${Fourthicon}' class='trash-icon' id='${entry.index}'>
         <img src='${Thirdicon}' class='dots' id='${entry.index}'>
@@ -48,6 +48,8 @@ class Manager {
     this.Options(moreIcon);  
     const disposeBtn = document.querySelectorAll('.trash-icon');
     this.listRemoval(disposeBtn);
+    const details = document.querySelectorAll('.detail');
+    this.update(details);
   }
 
   Options(moreIcon){
@@ -62,7 +64,6 @@ class Manager {
   listRemoval(disposeBtn) {
     disposeBtn.forEach((btn) => {
       btn.addEventListener('click', () => {
-        console.log('clicked');
         const index = btn.getAttribute('id');
         this.toDoList.splice(index, 1);
         if (this.toDoList.length > 0) {
@@ -74,6 +75,20 @@ class Manager {
         this.listRender();
       })
     })   
+  }
+
+  update(details) {
+    details.forEach((text) => {
+      text.addEventListener('keyup', (e) => {
+        if (e.key !== 'Enter' || !text.value) return;
+        else {
+          const index = text.getAttribute('id');          
+          this.toDoList[index].description = text.value;
+          localStorage.setItem('compiled', JSON.stringify(this.toDoList));
+          this.listRender();
+        }
+      })
+    })
   }
 }
 
